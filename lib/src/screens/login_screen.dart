1ab0017fabba4cfa3782a89751385ce11ec634fe
Candra Ramadhan Prasetya <candraramadhanp@gmail.com>
2022-09-16 20:40:48 +0700

    add media query

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -3,28 +3,55 @@ part of 'screens.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  Widget _buildHeaderText() {
-    return Column(
-      children: [
-        Text(
-          'Hello Again!',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+  Widget _buildHeaderText(Size size) {
+    return SizedBox(
+      width: size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SafeArea(
+            child: Text(
+              'Hello Again!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.black87,
+              ),
+            ),
           ),
-        ),
-      ],
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              'Welcome back you’ve been missed!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    //variable size
+    Size size = MediaQuery.of(context).size;
+    print(size.toString());
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //TODO Text hello dan welcome back
-
+          _buildHeaderText(size),
           //TODO Textfield untuk username dan password
-
+          SizedBox(
+            height: 24,
+          ),
           //TODO text recovery password
 
           //TODO Button Sign In
